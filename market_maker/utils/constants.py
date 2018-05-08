4d63c34f772ba414b0d8ecba72ea67ef1ed4e0d0
Ryan Fox <ryan@foxrow.com>
2018-05-08 14:17:55 -0500

    bump version number

--- market_maker/utils/constants.py
+++ market_maker/utils/constants.py
@@ -1,7 +1,7 @@
 import subprocess
 # Constants
 XBt_TO_XBT = 100000000
-VERSION = 'v1.1'
+VERSION = 'v1.4'
 try:
     VERSION = str(subprocess.check_output(["git", "describe", "--tags"], stderr=subprocess.DEVNULL).rstrip())
 except Exception as e:
