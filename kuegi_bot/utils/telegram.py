908d02a47e7f3129d09293160e2a1fe5c02815b4
kuegi <kuegi@turboprinz.de>
2020-07-26 18:10:43 +0200

    improved logging and dashboard

--- kuegi_bot/utils/telegram.py
+++ kuegi_bot/utils/telegram.py
@@ -5,27 +5,32 @@ class TelegramBot:
 	def __init__(self,logger,settings):
 		self.token= settings.token
 		self.logChatId= settings.logChatId
-		self.signalChannel= settings.signalChannel
+		self.executionChannel= settings.executionChannel
 		self.logger= logger
 		self.timer= None
 		self.messagesToSend = {}
 
 	def send_log(self,log_message,debounceId:str= None):
-		if debounceId is None:
-			debounceId= log_message
 		if self.logChatId is None:
 			self.logger.warn("missing telegram logChatId")
 			return
+
+		self.__internal_send(self.executionChannel, log_message)
+		''' 
+		# if doing debounce, but might have problems with id collision on multiple ids
+		if debounceId is None:
+			debounceId= log_message
 		if self.timer is not None:
 			self.timer.cancel()
 
 		self.timer= threading.Timer(interval=35, function= self.__internal_send_logs)
 		self.timer.start()
 		self.messagesToSend[debounceId] = log_message
+		'''
 
 	def send_execution(self, signal_message):
-		if self.signalChannel is not None:
-			self.__internal_send(self.signalChannel,signal_message)
+		if self.executionChannel is not None:
+			self.__internal_send(self.executionChannel, signal_message)
 
 	def __internal_send_logs(self):
 		self.timer= None
