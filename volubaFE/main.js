b4c8955742139fe78d4024ef122488e16995ebe5
kuegi <kuegi@turboprinz.de>
2020-12-02 20:29:10 +0100

    [voluba] added single-exchange cvds

--- volubaFE/main.js
+++ volubaFE/main.js
@@ -2,6 +2,7 @@ var barSeries;
 var cvdSeries;
 var volumeSeries;
 var volDeltaSeries;
+var exchangeSeries= {};
 
 var m1Data;
 
@@ -23,9 +24,10 @@ function aggregateM1Data() {
         var bar= { tstamp: barTstamp,
         open: 0, high:0, low:999999999, close:0, buyVolume:0, sellVolume:0, volume:0 };
         var count= 0;
+        bar.cvdByExchange= {};
         for(let exchange in b.barsByExchange) {
+            var exBar= b.barsByExchange[exchange];
             if(wantedExchanges.has(exchange)) {
-                var exBar= b.barsByExchange[exchange];
                 bar.open += exBar.open;
                 bar.high = Math.max(bar.high,exBar.high);
                 bar.low= Math.min(bar.low,exBar.low);
@@ -35,6 +37,7 @@ function aggregateM1Data() {
                 bar.sellVolume += exBar.sellVolume;
                 count++;
             }
+            bar.cvdByExchange[exchange]= exBar.buyVolume-exBar.sellVolume;
         }
         if(count > 0) {
             bar.open /= count;
@@ -50,6 +53,7 @@ function aggregateM1Data() {
             existing.volume += bar.volume;
             existing.buyVolume += bar.buyVolume;
             existing.sellVolume += bar.sellVolume;
+            existing.cvdByExchange = bar.cvdByExchange;
         } else {
             result.push(bar);
             lastTstamp= bar.tstamp;
@@ -85,7 +89,7 @@ function init() {
         }
     });
     cvdSeries = chart.addLineSeries({ priceScaleId: 'left' ,
-                priceLineVisible:false,
+                priceLineVisible:false
             });
     barSeries= chart.addCandlestickSeries({ priceScaleId: 'right' });
     volDeltaSeries = chart.addHistogramSeries({
@@ -125,20 +129,52 @@ function refreshExchanges(m1Data) {
             possibleExchanges.add(exchange);
         }
     });
-
+    colors= [
+    "Navy",
+    "Aqua",
+    "Fuchsia",
+    "Lime",
+    "Orange",
+    "Maroon",
+    "Purple",
+    "Blue",
+    "Teal",
+    "Green",
+    "Yellow",
+    "Olive",
+    "Red"
+    ];
+    var colorIdx= 0;
     var exchanges= document.getElementById("exchanges");
     exchanges.innerHTML= "";
     possibleExchanges.forEach(exchange => {
+        var hue= Math.round(Math.random()*360);
+        var color= colors[(colorIdx++) % colors.length];
         var label = document.createElement("label");
         var input= document.createElement("input");
+        var rect= document.createElement("div");
         input.type= "checkbox";
         input.value= exchange;
-        input.name="exchange";
+        input.name= "exchange";
+        input.id= exchange;
         input.checked= true;
         input.onchange= reinitData;
         label.innerHTML = exchange;
+        label.htmlFor= exchange;
+        rect.style.background= color;
+        rect.className="exchangeColor";
         exchanges.appendChild(input);
         exchanges.appendChild(label);
+        label.appendChild(rect);
+
+
+        exchangeSeries[exchange]= chart.addLineSeries({ priceScaleId: exchange+"cvds" ,
+                priceLineVisible:false,
+                lastValueVisible: false,
+                color:rect.style.background,
+                lineWidth:1
+
+            });
     });
 }
 
@@ -147,6 +183,9 @@ function checkWantedExchanges() {
     document.getElementsByName("exchange").forEach(input => {
         if(input.checked) {
             wantedExchanges.add(input.value);
+            exchangeSeries[input.value].applyOptions({visible:true});
+        } else {
+            exchangeSeries[input.value].applyOptions({visible:false});
         }
     });
 }
@@ -181,13 +220,30 @@ function fillSeries() {
     var cvds= [];
     var volumes= [];
     var volumeDeltas= [];
+    var exchangeCvdsData= {};
     var cvd= 0;
+    element= document.getElementById("showPrice");
+    barSeries.applyOptions({visible:element.checked});
+
+    element= document.getElementById("exchangeCvds");
+    wantedExchanges.forEach(exchange => {
+            exchangeSeries[exchange].applyOptions({visible:element.checked});
+    })
     aggregateM1Data(m1Data).forEach(b => {
         var bar= { time: b.tstamp,
         open: b.open, high:b.high, low:b.low, close:b.close };
         cvd += b.buyVolume-b.sellVolume
         candles.push(bar);
         cvds.push( { time: b.tstamp, value: cvd });
+
+        for(let exchange in b.cvdByExchange) {
+            if(!(exchange in exchangeCvdsData)) {
+                exchangeCvdsData[exchange]= {cvd:0,series:[]};
+            }
+            exchangeCvdsData[exchange].cvd += b.cvdByExchange[exchange];
+            exchangeCvdsData[exchange].series.push( { time: b.tstamp,
+                                                      value: exchangeCvdsData[exchange].cvd });
+        }
         if(b.buyVolume > b.sellVolume) {
             volumeDeltas.push( { time: b.tstamp, value:b.buyVolume-b.sellVolume, color: volumeBuy });
         } else {
@@ -200,6 +256,10 @@ function fillSeries() {
     cvdSeries.setData(cvds);
     volumeSeries.setData(volumes);
     volDeltaSeries.setData(volumeDeltas);
+
+    wantedExchanges.forEach(exchange =>{
+        exchangeSeries[exchange].setData(exchangeCvdsData[exchange].series);
+    });
 }
 
 function integrateNewM1Data(newData) {
