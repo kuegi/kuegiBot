fa5e94ab22828c1ec1c5544b0700280dc5bd5d54
Stephan Wolkow <stephan.wolkow@gmail.com>
2024-10-13 19:07:52 +0200

    added new variables to the settings

--- kuegi_bot/utils/helper.py
+++ kuegi_bot/utils/helper.py
@@ -58,7 +58,7 @@ def history_file_name(index, exchange,symbol='') :
 known_history_files= {
     "bitmex_XBTUSD": 49,
     "bybit_BTCUSD": 61,
-    "bybit_ETHUSD":57,
+    "bybit_ETHUSD":60,
     "bybit_XRPUSD":14,
     "bybit_BTCUSDM21":1,
     "bybit-linear_BTCUSDT":43,
